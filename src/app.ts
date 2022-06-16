c21b62a049cde9d89645cdf35788a18f099eeec5
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-16 16:15:46 +0700

    add IT and Accounting Department class

--- src/app.ts
+++ src/app.ts
@@ -16,19 +16,48 @@ class Department {
         console.log(this.employee);
     }
     addEmployee (name: string) {
-        this.id = 'd2';
         this.employee.push(name)
     }
 }
 
-const accounting = new Department ('Accounting','d1');
+// Define a IT Department class
+class ITDepartment extends Department {
+    constructor (id: string, public admins: string[]) {
+        super('IT', id);
+    }
+}
 
-accounting.addEmployee('Bao')
-accounting.addEmployee('Thi')
+class AccountingDepartment extends Department {
+    constructor (id: string, private report: string[]) {
+        super('Accounting', id);
+    }
+
+    addReport (text: string) {
+        this.report.push(text);
+    }
+
+    printReport () {
+        console.log(this.report)
+    }
+}
+
+
+const it = new ITDepartment ('d1', ['Bao']);
+const accounting = new AccountingDepartment ('d2', ['Times up!']);
+
+it.addEmployee('Truong')
+it.addEmployee('Thi')
+it.addEmployee('Phuoc')
 // accounting.employee[2] = 'Truong'
 
-accounting.describe();
-accounting.printEmployeeInformation();
+it.describe();
+// it.name = 'New Name';
+it.printEmployeeInformation();
+console.log(it)
+
+accounting.addReport('Something went wrong.....');
+accounting.printReport()
+
 
 // const accountingCopy = {
 //     describe: accounting.describe,
