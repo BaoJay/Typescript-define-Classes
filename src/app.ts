f8c6dc386ac95ec1a460ad91735f4cf2c523afe7
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-16 14:46:48 +0700

    add printEmployeeInformation and addEmployee methods

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 class Department {
     name: string;
+    employee: string[] = [];
+
     constructor(name: string) {
         this.name = name;
     }
@@ -7,13 +9,26 @@ class Department {
     describe (this: Department) {
         console.log("Department: ", this.name)
     }
+    printEmployeeInformation(this: Department) {
+        console.log(this.employee.length);
+        console.log(this.employee);
+    }
+    addEmployee (name: string) {
+        this.employee.push(name)
+    }
 }
 
 const accounting = new Department ('Accounting');
-accounting.describe();
 
-const accountingCopy = {
-    describe: accounting.describe,
-    name: 'Copy Accounting'
-}
-accountingCopy.describe();
+accounting.addEmployee('Bao')
+accounting.addEmployee('Thi')
+accounting.employee[2] = 'Truong'
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = {
+//     describe: accounting.describe,
+//     name: 'Copy Accounting'
+// }
+// accountingCopy.describe();
